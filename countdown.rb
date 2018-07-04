60c883236a1d682de39ff0ac0a237e5269c52a31
Christine Xiang <yutingcxiang@gmail.com>
2018-07-04 05:09:05 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,10 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
